ed57e00a87d360e51fe8a454b53ba26deed1205b
tedsuo <ted@radicaldesigns.org>
2012-09-09 19:55:12 -0700

    added require.js support

--- ref_binder.js
+++ ref_binder.js
@@ -1,30 +1,35 @@
-var RefBinder = function(target){
-  this._ref = {};
-  this.target = target;
-}
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
+define( function(require){
 
-module.exports = RefBinder;
-
-RefBinder.prototype.get = function(name,obj,events){
-  return this._ref[name];
-};
-
-RefBinder.prototype.set = function(name,obj,events){
-  this.unset(name);
-  for(var e_name in events){
-    obj.on( e_name, this.target[events[e_name]], this.target);
+  var RefBinder = function(target){
+    this._ref = {};
+    this.target = target;
   }
-  this._ref[name] = obj;
-};
 
-RefBinder.prototype.unset = function(name){
-  if(!this._ref[name]) return;
-  this._ref[name].off(null,null,this);
-  delete this._ref[name];
-};
+  RefBinder.prototype.get = function(name,obj,events){
+    return this._ref[name];
+  };
 
-RefBinder.prototype.unsetAll = function(){
-  for(var name in this._ref){
+  RefBinder.prototype.set = function(name,obj,events){
     this.unset(name);
-  }
-};
+    for(var e_name in events){
+      obj.on( e_name, this.target[events[e_name]], this.target);
+    }
+    this._ref[name] = obj;
+  };
+
+  RefBinder.prototype.unset = function(name){
+    if(!this._ref[name]) return;
+    this._ref[name].off(null,null,this);
+    delete this._ref[name];
+  };
+
+  RefBinder.prototype.unsetAll = function(){
+    for(var name in this._ref){
+      this.unset(name);
+    }
+  };
+
+  return RefBinder;
+
+});
